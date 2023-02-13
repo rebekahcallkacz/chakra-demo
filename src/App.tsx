0b65ba389e0e572211da2b4c3f3cba7522e0c4d9
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-02-13 18:50:55 -0500

    remove css from create react app template

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header>
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
