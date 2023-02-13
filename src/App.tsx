56c34a071a235b7776bfb628d837767cea21d478
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-02-13 18:59:34 -0500

    clarifying comment

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,13 @@ import {
 } from "@chakra-ui/react";
 
 function App() {
+  // Chakra UI has defined their own components for most html elements
+  // For example
+  // Box is div
+  // Text is p
+  // Heading is h
+  // Link is a
+  // They also have components for specific types of styling such as Flex and Center
   return (
     <ChakraProvider>
       <Box>
