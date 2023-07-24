31368b7d90aca879515e6a1a89d7f3c4a0d7f187
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-07-24 08:02:59 -0400

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {
   Center,
   ChakraProvider,
   CloseButton,
-  Flex,
+  Code,
   Heading,
   HStack,
   Link,
@@ -22,13 +22,14 @@ function App() {
   const [clickCount, setClickCount] = useState<number>(0);
   // This is a Chakra UI hook - they've built quite a few based on React
   // that you can use for common situations where you need a hook with a specific type of behavior
+  // This one is useDisclosure: https://chakra-ui.com/docs/hooks/use-disclosure
   const {
     isOpen: isAlertVisible,
     onClose: onAlertClose,
     onOpen: onAlertOpen,
   } = useDisclosure({ defaultIsOpen: false });
 
-  // This is also just react, but we're calling a function that's part of the Chakra UI hook useDisclosure
+  // This is also just React, but we're calling a function that's part of the Chakra UI hook useDisclosure
   useEffect(() => {
     if (clickCount === 3) {
       onAlertOpen();
@@ -53,42 +54,46 @@ function App() {
         They also have components for specific types of styling such as Flex, HStack and Center
       */}
       <Center>
-        <Heading textAlign={"center"}>
-          <Text fontSize="6xl">
-            Edit <code>src/App.tsx</code> and save to reload.
+        {/* I am styling my components using Chakra's style props: https://chakra-ui.com/docs/styled-system/style-props */}
+        {/* green.500 is a default color:  https://chakra-ui.com/docs/styled-system/theme#colors*/}
+        {/* size is also a default: https://chakra-ui.com/docs/styled-system/theme#colors */}
+        <Heading size="2xl" color="green.500" textAlign={"center"}>
+          <Text>
+            Edit <Code>src/App.tsx</Code> and save to reload.
           </Text>
-          <Link href="https://reactjs.org" rel="noopener noreferrer" isExternal>
+          <Link
+            href="https://reactjs.org"
+            rel="noopener noreferrer"
+            textDecoration="underline"
+            size="md"
+            isExternal
+          >
             Learn React
           </Link>
         </Heading>
       </Center>
-      <Flex>
-        <HStack>
-          {/* For any component you want to use, you can review it in the documentation
+      {/* Stack allows for stacking items horizontally or vertically: https://chakra-ui.com/docs/components/stack/usage */}
+      <HStack>
+        {/* For any component you want to use, you can review it in the documentation
         The default page will have some code snippets and the general functionality 
         The props tab will have any props that Chakra has added in addition to the html element props that already exist
         The theming tab will give you guidance on how to customize style
         For example, on this button, we're using a default color scheme from the theme to change the button color*/}
-          <Button colorScheme="cyan" onClick={() => handleClick()}>
-            Click Me!
-          </Button>
-          <Text>{`You have clicked ${clickCount} times.`}</Text>
-        </HStack>
-      </Flex>
+        <Button colorScheme="cyan" onClick={() => handleClick()}>
+          Click Me!
+        </Button>
+        <Text>{`You have clicked ${clickCount} times.`}</Text>
+      </HStack>
       {isAlertVisible && (
-        <Alert status="warning">
+        // width="max" is "max-content": https://chakra-ui.com/docs/styled-system/theme#sizes
+        // Alert comes with four related components - you can choose which one(s) you want to use
+        <Alert status="warning" width="max" marginTop={5}>
           <AlertIcon />
           <Box>
             <AlertTitle>Warning!</AlertTitle>
             <AlertDescription>You have clicked 3 times.</AlertDescription>
           </Box>
-          <CloseButton
-            alignSelf="flex-start"
-            position="relative"
-            right={-1}
-            top={-1}
-            onClick={onAlertClose}
-          />
+          <CloseButton right={-1} top={-1} onClick={onAlertClose} />
         </Alert>
       )}
     </ChakraProvider>
